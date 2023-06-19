816ce9af335c3961a7f2c192f6c2af5d35b98486
Nine <davidheetebrij@gmail.com>
2023-06-19 16:18:08 +0200

    better quicksort

--- Assets/Scripts/S_QuickSort.cs
+++ Assets/Scripts/S_QuickSort.cs
@@ -13,12 +13,6 @@ public class S_QuickSort : S_SortingMethod
     private int _pivot;
 
     [SerializeField, ReadOnly]
-    private int _partisionIndex = 0;
-
-    [SerializeField, ReadOnly]
-    private int _partisionSize;
-
-    [SerializeField, ReadOnly]
     int iLow = 0;
     [SerializeField, ReadOnly]
     int iHigh = 0;
@@ -26,6 +20,10 @@ public class S_QuickSort : S_SortingMethod
     public override void Setup(int[] pArray)
     {
         base.Setup(pArray);
+
+        iLow = 0;
+        iHigh = Length - 1;
+
         /*iLow = Length - 1;
         iHigh = 0;
 
@@ -49,99 +47,57 @@ public class S_QuickSort : S_SortingMethod
 
     public override IEnumerator Sort(bool pLoop = true)
     {
-        do
+        int high = iHigh;
+        int p;
+
+        if (iLow >= 0 && iHigh >= 0 && iLow < iHigh)
         {
-            quicksort(Array, 0, Length-1);
-            //Step(Index);
-
-            if (Index >= Array.Length - 1)
-            {
-                IsDone = true;
-                break;
-            }
-
-
-            yield return null;
-        } while (true);
+            Step(Index);
+            p = Index;
+            iHigh = p;
+            yield return Sort(); // Note: the pivot is now included
+            iLow = p + 1;
+            iHigh = high;
+            yield return Sort();
+        }
     }
 
     protected override void Step(int pIndex)
     {
-
-
-        for (int i = iHigh; i < _partisionSize; i++)
-        {
-            if (Array[i] >= Array[_pivot])
-            {
-                iHigh = i;
-                break;
-            }
-        }
-
-        for (int j = iLow = _partisionSize - 1; j > 0; j--)
-        {
-            if (Array[j] < Array[_pivot])
-            {
-                iLow = j;
-                break;
-            }
-        }
-
-
-        if (iLow <= iHigh)
-        {
-            Swap(iHigh, _pivot);
-            //Partision(0, _pivot);
-        }
-        else
-            Swap(iLow, iHigh);
-
-
-        base.Step(pIndex);
-    }
-
-
-    // Sorts a (portion of an) array, divides it into partitions, then sorts those
-    private void quicksort(int[] A, int lo, int hi)
-    {
-        if (lo >= 0 && hi >= 0 && lo < hi)
-        {
-            int p = partition(A, lo, hi);
-            quicksort(A, lo, p); // Note: the pivot is now included
-            quicksort(A, p + 1, hi);
-        }
-    }
-
-    // Divides array into two partitions
-    private int partition(int[] A, int lo, int hi)
-    {
         // Pivot value
-        _pivot = A[((hi - lo) / 2) + lo]; // The value in the middle of the array
+        _pivot = Array[((iHigh - iLow) / 2) + iLow]; // The value in the middle of the array
 
         // Left index
-        int i = lo - 1;
+        int i = iLow - 1;
 
         // Right index
-        int j = hi + 1;
+        int j = iHigh + 1;
 
 
         while (true)
         {
             // Move the left index to the right at least once and while the element at
             // the left index is less than the pivot
-            do { i = i + 1; } while (A[i] < _pivot);
+            do { i = i + 1; } while (Array[i] < _pivot);
 
             // Move the right index to the left at least once and while the element at
             // the right index is greater than the pivot
-            do { j = j - 1; } while (A[j] > _pivot);
+            do { j = j - 1; } while (Array[j] > _pivot);
 
             // If the indices crossed, return
-            if (i >= j) return j;
-
-            // Swap the elements at the left and right indices
-            Swap(i, j);
+            if (i >= j)
+            {
+                Index = j;
+                break;
+            }
+            else
+            {
+                // Swap the elements at the left and right indices
+                Swap(i, j);
+            }
         }
+
+
+        base.Step(pIndex);
     }
-
-
 }
